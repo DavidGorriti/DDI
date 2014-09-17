b8fde2df4fd5ed29d9833ad70c8849543230cd6a
uesteibar <uesteibar@gmail.com>
2014-09-17 10:35:56 +0200

    EjerciciosBasicos1 hecho

--- Eval1/coding/Juego1/Juego1/Form1.vb
+++ Eval1/coding/Juego1/Juego1/Form1.vb
@@ -8,12 +8,10 @@
     End Sub
 
     Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        Dim nX As Integer = pctMushStatic.Location.X + 2
+        Dim nY As Integer = pctMushStatic.Location.Y + 2
 
-
-
-
-
-
+        pctMushStatic.Location = New Point(nX, nY)
 
     End Sub
 
