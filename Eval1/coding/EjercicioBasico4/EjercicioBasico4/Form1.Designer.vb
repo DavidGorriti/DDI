7d9820d73ae498e2d6fdfabba6fc815786fd14ce
uesteibar <uesteibar@gmail.com>
2014-09-17 12:37:41 +0200

    Empezado EjercicioBasico4. 80% terminado.

---
+++ Eval1/coding/EjercicioBasico4/EjercicioBasico4/Form1.Designer.vb
@@ -0,0 +1,485 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape5 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape4 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape3 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape2 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.lblHorasSIMYER = New System.Windows.Forms.Label()
+        Me.lblHorasADDAIG = New System.Windows.Forms.Label()
+        Me.lblHorasPELE = New System.Windows.Forms.Label()
+        Me.lblHorasRET = New System.Windows.Forms.Label()
+        Me.lblHorasINGLES = New System.Windows.Forms.Label()
+        Me.lblHorasCALIDAD = New System.Windows.Forms.Label()
+        Me.lblHorasDDAEE4G = New System.Windows.Forms.Label()
+        Me.lblHorasDYRSP = New System.Windows.Forms.Label()
+        Me.lblHorasFOL = New System.Windows.Forms.Label()
+        Me.lblHorasFCT = New System.Windows.Forms.Label()
+        Me.txtSIMYER = New System.Windows.Forms.TextBox()
+        Me.txtADDAIG = New System.Windows.Forms.TextBox()
+        Me.txtFCT = New System.Windows.Forms.TextBox()
+        Me.txtFOL = New System.Windows.Forms.TextBox()
+        Me.txtDDAEE4G = New System.Windows.Forms.TextBox()
+        Me.txtCALIDAD = New System.Windows.Forms.TextBox()
+        Me.txtINGLES = New System.Windows.Forms.TextBox()
+        Me.txtRET = New System.Windows.Forms.TextBox()
+        Me.txtPELE = New System.Windows.Forms.TextBox()
+        Me.txtDYRSP = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape5, Me.LineShape4, Me.LineShape3, Me.LineShape2, Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(502, 376)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape5
+        '
+        Me.LineShape5.Name = "LineShape5"
+        Me.LineShape5.X1 = 40
+        Me.LineShape5.X2 = 467
+        Me.LineShape5.Y1 = 299
+        Me.LineShape5.Y2 = 298
+        '
+        'LineShape4
+        '
+        Me.LineShape4.Name = "LineShape4"
+        Me.LineShape4.X1 = 40
+        Me.LineShape4.X2 = 468
+        Me.LineShape4.Y1 = 219
+        Me.LineShape4.Y2 = 219
+        '
+        'LineShape3
+        '
+        Me.LineShape3.Name = "LineShape3"
+        Me.LineShape3.X1 = 35
+        Me.LineShape3.X2 = 469
+        Me.LineShape3.Y1 = 71
+        Me.LineShape3.Y2 = 71
+        '
+        'LineShape2
+        '
+        Me.LineShape2.Name = "LineShape2"
+        Me.LineShape2.X1 = 365
+        Me.LineShape2.X2 = 365
+        Me.LineShape2.Y1 = 19
+        Me.LineShape2.Y2 = 335
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 207
+        Me.LineShape1.X2 = 207
+        Me.LineShape1.Y1 = 20
+        Me.LineShape1.Y2 = 337
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(42, 20)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(138, 39)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nombre"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(223, 20)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(108, 39)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Horas"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 25.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(373, 20)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(89, 39)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Nota"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(85, 82)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "SIMYER"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(85, 108)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(48, 13)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "ADDAIG"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(85, 175)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(46, 13)
+        Me.Label6.TabIndex = 6
+        Me.Label6.Text = "INGLÉS"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(85, 153)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(29, 13)
+        Me.Label7.TabIndex = 7
+        Me.Label7.Text = "RET"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(85, 230)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(58, 13)
+        Me.Label8.TabIndex = 8
+        Me.Label8.Text = "DDAEE4G"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(85, 198)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(53, 13)
+        Me.Label9.TabIndex = 9
+        Me.Label9.Text = "CALIDAD"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(85, 252)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(44, 13)
+        Me.Label10.TabIndex = 10
+        Me.Label10.Text = "DYRSP"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(85, 311)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(27, 13)
+        Me.Label11.TabIndex = 11
+        Me.Label11.Text = "FCT"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(85, 275)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(27, 13)
+        Me.Label12.TabIndex = 12
+        Me.Label12.Text = "FOL"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(85, 130)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(34, 13)
+        Me.Label13.TabIndex = 13
+        Me.Label13.Text = "PELE"
+        '
+        'lblHorasSIMYER
+        '
+        Me.lblHorasSIMYER.AutoSize = True
+        Me.lblHorasSIMYER.Location = New System.Drawing.Point(256, 82)
+        Me.lblHorasSIMYER.Name = "lblHorasSIMYER"
+        Me.lblHorasSIMYER.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasSIMYER.TabIndex = 14
+        Me.lblHorasSIMYER.Text = "202"
+        '
+        'lblHorasADDAIG
+        '
+        Me.lblHorasADDAIG.AutoSize = True
+        Me.lblHorasADDAIG.Location = New System.Drawing.Point(256, 108)
+        Me.lblHorasADDAIG.Name = "lblHorasADDAIG"
+        Me.lblHorasADDAIG.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasADDAIG.TabIndex = 15
+        Me.lblHorasADDAIG.Text = "259"
+        '
+        'lblHorasPELE
+        '
+        Me.lblHorasPELE.AutoSize = True
+        Me.lblHorasPELE.Location = New System.Drawing.Point(256, 130)
+        Me.lblHorasPELE.Name = "lblHorasPELE"
+        Me.lblHorasPELE.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasPELE.TabIndex = 16
+        Me.lblHorasPELE.Text = "317"
+        '
+        'lblHorasRET
+        '
+        Me.lblHorasRET.AutoSize = True
+        Me.lblHorasRET.Location = New System.Drawing.Point(256, 153)
+        Me.lblHorasRET.Name = "lblHorasRET"
+        Me.lblHorasRET.Size = New System.Drawing.Size(19, 13)
+        Me.lblHorasRET.TabIndex = 17
+        Me.lblHorasRET.Text = "60"
+        '
+        'lblHorasINGLES
+        '
+        Me.lblHorasINGLES.AutoSize = True
+        Me.lblHorasINGLES.Location = New System.Drawing.Point(256, 175)
+        Me.lblHorasINGLES.Name = "lblHorasINGLES"
+        Me.lblHorasINGLES.Size = New System.Drawing.Size(19, 13)
+        Me.lblHorasINGLES.TabIndex = 18
+        Me.lblHorasINGLES.Text = "58"
+        '
+        'lblHorasCALIDAD
+        '
+        Me.lblHorasCALIDAD.AutoSize = True
+        Me.lblHorasCALIDAD.Location = New System.Drawing.Point(256, 198)
+        Me.lblHorasCALIDAD.Name = "lblHorasCALIDAD"
+        Me.lblHorasCALIDAD.Size = New System.Drawing.Size(19, 13)
+        Me.lblHorasCALIDAD.TabIndex = 19
+        Me.lblHorasCALIDAD.Text = "40"
+        '
+        'lblHorasDDAEE4G
+        '
+        Me.lblHorasDDAEE4G.AutoSize = True
+        Me.lblHorasDDAEE4G.Location = New System.Drawing.Point(256, 230)
+        Me.lblHorasDDAEE4G.Name = "lblHorasDDAEE4G"
+        Me.lblHorasDDAEE4G.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasDDAEE4G.TabIndex = 20
+        Me.lblHorasDDAEE4G.Text = "265"
+        '
+        'lblHorasDYRSP
+        '
+        Me.lblHorasDYRSP.AutoSize = True
+        Me.lblHorasDYRSP.Location = New System.Drawing.Point(256, 252)
+        Me.lblHorasDYRSP.Name = "lblHorasDYRSP"
+        Me.lblHorasDYRSP.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasDYRSP.TabIndex = 21
+        Me.lblHorasDYRSP.Text = "208"
+        '
+        'lblHorasFOL
+        '
+        Me.lblHorasFOL.AutoSize = True
+        Me.lblHorasFOL.Location = New System.Drawing.Point(256, 275)
+        Me.lblHorasFOL.Name = "lblHorasFOL"
+        Me.lblHorasFOL.Size = New System.Drawing.Size(19, 13)
+        Me.lblHorasFOL.TabIndex = 22
+        Me.lblHorasFOL.Text = "60"
+        '
+        'lblHorasFCT
+        '
+        Me.lblHorasFCT.AutoSize = True
+        Me.lblHorasFCT.Location = New System.Drawing.Point(256, 311)
+        Me.lblHorasFCT.Name = "lblHorasFCT"
+        Me.lblHorasFCT.Size = New System.Drawing.Size(25, 13)
+        Me.lblHorasFCT.TabIndex = 23
+        Me.lblHorasFCT.Text = "304"
+        '
+        'txtSIMYER
+        '
+        Me.txtSIMYER.Location = New System.Drawing.Point(411, 80)
+        Me.txtSIMYER.Name = "txtSIMYER"
+        Me.txtSIMYER.Size = New System.Drawing.Size(31, 20)
+        Me.txtSIMYER.TabIndex = 24
+        '
+        'txtADDAIG
+        '
+        Me.txtADDAIG.Location = New System.Drawing.Point(411, 105)
+        Me.txtADDAIG.Name = "txtADDAIG"
+        Me.txtADDAIG.Size = New System.Drawing.Size(31, 20)
+        Me.txtADDAIG.TabIndex = 25
+        '
+        'txtFCT
+        '
+        Me.txtFCT.Location = New System.Drawing.Point(411, 308)
+        Me.txtFCT.Name = "txtFCT"
+        Me.txtFCT.ReadOnly = True
+        Me.txtFCT.Size = New System.Drawing.Size(31, 20)
+        Me.txtFCT.TabIndex = 26
+        '
+        'txtFOL
+        '
+        Me.txtFOL.Location = New System.Drawing.Point(411, 272)
+        Me.txtFOL.Name = "txtFOL"
+        Me.txtFOL.ReadOnly = True
+        Me.txtFOL.Size = New System.Drawing.Size(31, 20)
+        Me.txtFOL.TabIndex = 27
+        '
+        'txtDDAEE4G
+        '
+        Me.txtDDAEE4G.Location = New System.Drawing.Point(411, 227)
+        Me.txtDDAEE4G.Name = "txtDDAEE4G"
+        Me.txtDDAEE4G.ReadOnly = True
+        Me.txtDDAEE4G.Size = New System.Drawing.Size(31, 20)
+        Me.txtDDAEE4G.TabIndex = 28
+        '
+        'txtCALIDAD
+        '
+        Me.txtCALIDAD.Location = New System.Drawing.Point(411, 195)
+        Me.txtCALIDAD.Name = "txtCALIDAD"
+        Me.txtCALIDAD.Size = New System.Drawing.Size(31, 20)
+        Me.txtCALIDAD.TabIndex = 29
+        '
+        'txtINGLES
+        '
+        Me.txtINGLES.Location = New System.Drawing.Point(411, 172)
+        Me.txtINGLES.Name = "txtINGLES"
+        Me.txtINGLES.Size = New System.Drawing.Size(31, 20)
+        Me.txtINGLES.TabIndex = 30
+        '
+        'txtRET
+        '
+        Me.txtRET.Location = New System.Drawing.Point(411, 150)
+        Me.txtRET.Name = "txtRET"
+        Me.txtRET.Size = New System.Drawing.Size(31, 20)
+        Me.txtRET.TabIndex = 31
+        '
+        'txtPELE
+        '
+        Me.txtPELE.Location = New System.Drawing.Point(411, 127)
+        Me.txtPELE.Name = "txtPELE"
+        Me.txtPELE.Size = New System.Drawing.Size(31, 20)
+        Me.txtPELE.TabIndex = 32
+        '
+        'txtDYRSP
+        '
+        Me.txtDYRSP.Location = New System.Drawing.Point(411, 249)
+        Me.txtDYRSP.Name = "txtDYRSP"
+        Me.txtDYRSP.ReadOnly = True
+        Me.txtDYRSP.Size = New System.Drawing.Size(31, 20)
+        Me.txtDYRSP.TabIndex = 33
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(502, 376)
+        Me.Controls.Add(Me.txtDYRSP)
+        Me.Controls.Add(Me.txtPELE)
+        Me.Controls.Add(Me.txtRET)
+        Me.Controls.Add(Me.txtINGLES)
+        Me.Controls.Add(Me.txtCALIDAD)
+        Me.Controls.Add(Me.txtDDAEE4G)
+        Me.Controls.Add(Me.txtFOL)
+        Me.Controls.Add(Me.txtFCT)
+        Me.Controls.Add(Me.txtADDAIG)
+        Me.Controls.Add(Me.txtSIMYER)
+        Me.Controls.Add(Me.lblHorasFCT)
+        Me.Controls.Add(Me.lblHorasFOL)
+        Me.Controls.Add(Me.lblHorasDYRSP)
+        Me.Controls.Add(Me.lblHorasDDAEE4G)
+        Me.Controls.Add(Me.lblHorasCALIDAD)
+        Me.Controls.Add(Me.lblHorasINGLES)
+        Me.Controls.Add(Me.lblHorasRET)
+        Me.Controls.Add(Me.lblHorasPELE)
+        Me.Controls.Add(Me.lblHorasADDAIG)
+        Me.Controls.Add(Me.lblHorasSIMYER)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape5 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape4 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape3 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape2 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents lblHorasSIMYER As System.Windows.Forms.Label
+    Friend WithEvents lblHorasADDAIG As System.Windows.Forms.Label
+    Friend WithEvents lblHorasPELE As System.Windows.Forms.Label
+    Friend WithEvents lblHorasRET As System.Windows.Forms.Label
+    Friend WithEvents lblHorasINGLES As System.Windows.Forms.Label
+    Friend WithEvents lblHorasCALIDAD As System.Windows.Forms.Label
+    Friend WithEvents lblHorasDDAEE4G As System.Windows.Forms.Label
+    Friend WithEvents lblHorasDYRSP As System.Windows.Forms.Label
+    Friend WithEvents lblHorasFOL As System.Windows.Forms.Label
+    Friend WithEvents lblHorasFCT As System.Windows.Forms.Label
+    Friend WithEvents txtSIMYER As System.Windows.Forms.TextBox
+    Friend WithEvents txtADDAIG As System.Windows.Forms.TextBox
+    Friend WithEvents txtFCT As System.Windows.Forms.TextBox
+    Friend WithEvents txtFOL As System.Windows.Forms.TextBox
+    Friend WithEvents txtDDAEE4G As System.Windows.Forms.TextBox
+    Friend WithEvents txtCALIDAD As System.Windows.Forms.TextBox
+    Friend WithEvents txtINGLES As System.Windows.Forms.TextBox
+    Friend WithEvents txtRET As System.Windows.Forms.TextBox
+    Friend WithEvents txtPELE As System.Windows.Forms.TextBox
+    Friend WithEvents txtDYRSP As System.Windows.Forms.TextBox
+
+End Class
---
+++ Eval1/coding/EjercicioBasico4/EjercicioBasico4/Form1.vb
@@ -0,0 +1,183 @@
+﻿Public Class Form1
+
+    Private Property checkCurso1 As Boolean
+
+    Private Sub curso1(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtSIMYER.TextChanged, txtADDAIG.TextAlignChanged, txtCALIDAD.TextChanged, txtINGLES.TextChanged, txtPELE.TextChanged, txtRET.TextChanged
+        Dim aprobado As Boolean = checkCurso1()
+
+        If aprobado Then
+            activateCurso2()
+        End If
+
+    End Sub
+
+    Function checkCurso1e()
+        Dim horasSusp As Integer = 0
+        Dim asignSusp As Integer = 0
+
+        If IsNumeric(txtSIMYER.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtSIMYER.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtADDAIG.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtADDAIG.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtCALIDAD.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtCALIDAD.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtINGLES.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtINGLES.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtPELE.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtPELE.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtRET.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtRET.Text = ""
+            Return False
+        End If
+
+
+        If txtSIMYER.Text < 5 Then
+            horasSusp = horasSusp + lblHorasSIMYER.Text
+            asignSusp = asignSusp + 1
+        End If
+        If txtADDAIG.Text < 5 Then
+            horasSusp = horasSusp + lblHorasADDAIG.Text
+            asignSusp = asignSusp + 1
+        End If
+        If txtCALIDAD.Text < 5 Then
+            horasSusp = horasSusp + lblHorasCALIDAD.Text
+            asignSusp = asignSusp + 1
+        End If
+        If txtINGLES.Text < 5 Then
+            horasSusp = horasSusp + lblHorasINGLES.Text
+            asignSusp = asignSusp + 1
+        End If
+        If txtPELE.Text < 5 Then
+            horasSusp = horasSusp + lblHorasPELE.Text
+            asignSusp = asignSusp + 1
+        End If
+        If txtRET.Text < 5 Then
+            horasSusp = horasSusp + lblHorasRET.Text
+            asignSusp = asignSusp + 1
+        End If
+
+        If asignSusp > 3 Or horasSusp > 300 Then
+            Return False
+        Else
+            Return True
+        End If
+
+    End Function
+
+    Private Sub activateCurso2()
+        txtDDAEE4G.ReadOnly = False
+        txtDYRSP.ReadOnly = False
+        txtFOL.ReadOnly = False
+    End Sub
+
+
+    Private Sub curso2(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtDDAEE4G.TextChanged, txtDYRSP.TextAlignChanged, txtFOL.TextChanged
+        Dim aprobado As Boolean = checkCurso2()
+
+        If aprobado Then
+            activateCurso2()
+        End If
+
+    End Sub
+
+    Private Function checkCurso2() As Boolean
+        Dim horasSusp As Integer = 0
+        Dim asignSusp As Integer = 0
+
+        If IsNumeric(txtSIMYER.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtSIMYER.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtADDAIG.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtADDAIG.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtCALIDAD.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtCALIDAD.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtINGLES.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtINGLES.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtPELE.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtPELE.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtRET.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtRET.Text = ""
+            Return False
+        End If
+
+
+        If txtSIMYER.Text < 5 Then
+            Return False
+        End If
+        If txtADDAIG.Text < 5 Then
+            Return False
+        End If
+        If txtCALIDAD.Text < 5 Then
+            Return False
+        End If
+        If txtINGLES.Text < 5 Then
+            Return False
+        End If
+        If txtPELE.Text < 5 Then
+            Return False
+        End If
+        If txtRET.Text < 5 Then
+            Return False
+        End If
+
+
+
+        If IsNumeric(txtDDAEE4G.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtDDAEE4G.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtDYRSP.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtDYRSP.Text = ""
+            Return False
+        End If
+        If IsNumeric(txtFOL.Text) Then
+            MsgBox("No puede introducir una letra como nota")
+            txtFOL.Text = ""
+            Return False
+        End If
+
+        If txtDDAEE4G.Text < 5 Then
+            Return False
+        End If
+        If txtDYRSP.Text < 5 Then
+            Return False
+        End If
+        If txtFOL.Text < 5 Then
+            Return False
+        End If
+    End Function
+
+End Class
