1eb85b4600f449f96540b0269a18ff254245ba2d
uesteibar <uesteibar@gmail.com>
2014-09-17 10:55:30 +0200

    EjercicioBasico2 terminado

---
+++ Eval1/coding/EjercicioBasico2/EjercicioBasico2/Form1.Designer.vb
@@ -0,0 +1,243 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtCantidad = New System.Windows.Forms.TextBox()
+        Me.txtPVP = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.txtDescuento = New System.Windows.Forms.TextBox()
+        Me.txtIVA = New System.Windows.Forms.TextBox()
+        Me.txtTotalDescuento = New System.Windows.Forms.TextBox()
+        Me.txtTotalIVA = New System.Windows.Forms.TextBox()
+        Me.txtImporte = New System.Windows.Forms.TextBox()
+        Me.lblCantidad = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.btnSalir = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtCantidad
+        '
+        Me.txtCantidad.Location = New System.Drawing.Point(94, 29)
+        Me.txtCantidad.Name = "txtCantidad"
+        Me.txtCantidad.Size = New System.Drawing.Size(139, 20)
+        Me.txtCantidad.TabIndex = 0
+        '
+        'txtPVP
+        '
+        Me.txtPVP.Location = New System.Drawing.Point(94, 65)
+        Me.txtPVP.Name = "txtPVP"
+        Me.txtPVP.Size = New System.Drawing.Size(139, 20)
+        Me.txtPVP.TabIndex = 1
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Location = New System.Drawing.Point(94, 101)
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.ReadOnly = True
+        Me.txtTotal.Size = New System.Drawing.Size(139, 20)
+        Me.txtTotal.TabIndex = 2
+        '
+        'txtDescuento
+        '
+        Me.txtDescuento.Location = New System.Drawing.Point(116, 150)
+        Me.txtDescuento.Name = "txtDescuento"
+        Me.txtDescuento.Size = New System.Drawing.Size(48, 20)
+        Me.txtDescuento.TabIndex = 3
+        '
+        'txtIVA
+        '
+        Me.txtIVA.Location = New System.Drawing.Point(116, 189)
+        Me.txtIVA.Name = "txtIVA"
+        Me.txtIVA.Size = New System.Drawing.Size(48, 20)
+        Me.txtIVA.TabIndex = 4
+        '
+        'txtTotalDescuento
+        '
+        Me.txtTotalDescuento.Location = New System.Drawing.Point(273, 150)
+        Me.txtTotalDescuento.Name = "txtTotalDescuento"
+        Me.txtTotalDescuento.ReadOnly = True
+        Me.txtTotalDescuento.Size = New System.Drawing.Size(139, 20)
+        Me.txtTotalDescuento.TabIndex = 5
+        '
+        'txtTotalIVA
+        '
+        Me.txtTotalIVA.Location = New System.Drawing.Point(273, 189)
+        Me.txtTotalIVA.Name = "txtTotalIVA"
+        Me.txtTotalIVA.ReadOnly = True
+        Me.txtTotalIVA.Size = New System.Drawing.Size(139, 20)
+        Me.txtTotalIVA.TabIndex = 6
+        '
+        'txtImporte
+        '
+        Me.txtImporte.Location = New System.Drawing.Point(116, 277)
+        Me.txtImporte.Name = "txtImporte"
+        Me.txtImporte.ReadOnly = True
+        Me.txtImporte.Size = New System.Drawing.Size(139, 20)
+        Me.txtImporte.TabIndex = 7
+        '
+        'lblCantidad
+        '
+        Me.lblCantidad.AutoSize = True
+        Me.lblCantidad.Location = New System.Drawing.Point(37, 35)
+        Me.lblCantidad.Name = "lblCantidad"
+        Me.lblCantidad.Size = New System.Drawing.Size(49, 13)
+        Me.lblCantidad.TabIndex = 8
+        Me.lblCantidad.Text = "Cantidad"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(109, 355)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 9
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'btnSalir
+        '
+        Me.btnSalir.Location = New System.Drawing.Point(231, 355)
+        Me.btnSalir.Name = "btnSalir"
+        Me.btnSalir.Size = New System.Drawing.Size(75, 23)
+        Me.btnSalir.TabIndex = 10
+        Me.btnSalir.Text = "Salir"
+        Me.btnSalir.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(37, 68)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(34, 13)
+        Me.Label1.TabIndex = 11
+        Me.Label1.Text = "P.V.P"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(37, 108)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(31, 13)
+        Me.Label2.TabIndex = 12
+        Me.Label2.Text = "Total"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(37, 153)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(73, 13)
+        Me.Label3.TabIndex = 13
+        Me.Label3.Text = "Descuento(%)"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(37, 192)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 13)
+        Me.Label4.TabIndex = 14
+        Me.Label4.Text = "IVA (%)"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(181, 153)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(86, 13)
+        Me.Label5.TabIndex = 15
+        Me.Label5.Text = "Total Descuento"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(182, 192)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(51, 13)
+        Me.Label6.TabIndex = 16
+        Me.Label6.Text = "Total IVA"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(35, 280)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(69, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "Total Importe"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(443, 423)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.btnSalir)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.lblCantidad)
+        Me.Controls.Add(Me.txtImporte)
+        Me.Controls.Add(Me.txtTotalIVA)
+        Me.Controls.Add(Me.txtTotalDescuento)
+        Me.Controls.Add(Me.txtIVA)
+        Me.Controls.Add(Me.txtDescuento)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtPVP)
+        Me.Controls.Add(Me.txtCantidad)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents txtCantidad As System.Windows.Forms.TextBox
+    Friend WithEvents txtPVP As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotal As System.Windows.Forms.TextBox
+    Friend WithEvents txtDescuento As System.Windows.Forms.TextBox
+    Friend WithEvents txtIVA As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotalDescuento As System.Windows.Forms.TextBox
+    Friend WithEvents txtTotalIVA As System.Windows.Forms.TextBox
+    Friend WithEvents txtImporte As System.Windows.Forms.TextBox
+    Friend WithEvents lblCantidad As System.Windows.Forms.Label
+    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents btnSalir As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ Eval1/coding/EjercicioBasico2/EjercicioBasico2/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub btnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+        txtTotal.Text = txtCantidad.Text * txtPVP.Text
+        txtTotalDescuento.Text = txtTotal.Text * (txtDescuento.Text / 100)
+        txtTotalIVA.Text = txtTotal.Text * (txtIVA.Text / 100)
+
+        txtImporte.Text = txtTotal.Text - txtTotalDescuento.Text + txtTotalIVA.Text
+    End Sub
+
+    Private Sub btnSalir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSalir.Click
+        Me.Close()
+
+    End Sub
+End Class
