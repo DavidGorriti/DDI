2514f31f4e6a5947216aa8158ce75b68f0237162
uesteibar <uesteibar@gmail.com>
2014-09-24 11:11:54 +0200

    chkEjercicio terminado

--- Eval1/coding/chkEjercicio/chkEjercicio/Form1.vb
+++ Eval1/coding/chkEjercicio/chkEjercicio/Form1.vb
@@ -1,21 +1,24 @@
 ﻿Public Class Form1
+    Dim contador As Integer = 0
 
     Private Sub checked(ByVal sender As Object, ByVal e As EventArgs) Handles _
         chk1.CheckedChanged, chk2.CheckedChanged, chk3.CheckedChanged, chk4.CheckedChanged, chk5.CheckedChanged
 
 
-        Dim contador As Integer = 0
+
         Dim chkControl As CheckBox
 
-        For Each chkControl In grp1.Controls
-            If chkControl.Checked Then
-                contador = contador + 1
-            End If
-        Next
+        
+
+        chkControl = sender
+        If chkControl.Checked Then
+            contador = contador + 1
+        Else
+            contador = contador - 1
+        End If
 
 
         If contador > 3 Then
-            chkControl = sender
             chkControl.Checked = False
         End If
 
