2b5f1900154305790556a50ff321d9f037874783
uesteibar <uesteibar@gmail.com>
2014-10-01 12:24:22 +0200

    ejercicio tres en raya hecho

---
+++ Eval1/coding/tresEnRaya/tresEnRaya/Form1.Designer.vb
@@ -0,0 +1,201 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.grpbJuego = New System.Windows.Forms.GroupBox()
+        Me.PictureBox9 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox8 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox7 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox6 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.ImageList = New System.Windows.Forms.ImageList(Me.components)
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.grpbJuego.SuspendLayout()
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'grpbJuego
+        '
+        Me.grpbJuego.Controls.Add(Me.PictureBox9)
+        Me.grpbJuego.Controls.Add(Me.PictureBox8)
+        Me.grpbJuego.Controls.Add(Me.PictureBox7)
+        Me.grpbJuego.Controls.Add(Me.PictureBox6)
+        Me.grpbJuego.Controls.Add(Me.PictureBox5)
+        Me.grpbJuego.Controls.Add(Me.PictureBox4)
+        Me.grpbJuego.Controls.Add(Me.PictureBox3)
+        Me.grpbJuego.Controls.Add(Me.PictureBox2)
+        Me.grpbJuego.Controls.Add(Me.PictureBox1)
+        Me.grpbJuego.Location = New System.Drawing.Point(12, 12)
+        Me.grpbJuego.Name = "grpbJuego"
+        Me.grpbJuego.Size = New System.Drawing.Size(294, 205)
+        Me.grpbJuego.TabIndex = 0
+        Me.grpbJuego.TabStop = False
+        '
+        'PictureBox9
+        '
+        Me.PictureBox9.Location = New System.Drawing.Point(57, 137)
+        Me.PictureBox9.Name = "PictureBox9"
+        Me.PictureBox9.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox9.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox9.TabIndex = 8
+        Me.PictureBox9.TabStop = False
+        '
+        'PictureBox8
+        '
+        Me.PictureBox8.Location = New System.Drawing.Point(117, 137)
+        Me.PictureBox8.Name = "PictureBox8"
+        Me.PictureBox8.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox8.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox8.TabIndex = 7
+        Me.PictureBox8.TabStop = False
+        '
+        'PictureBox7
+        '
+        Me.PictureBox7.Location = New System.Drawing.Point(177, 137)
+        Me.PictureBox7.Name = "PictureBox7"
+        Me.PictureBox7.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox7.TabIndex = 6
+        Me.PictureBox7.TabStop = False
+        '
+        'PictureBox6
+        '
+        Me.PictureBox6.Location = New System.Drawing.Point(117, 81)
+        Me.PictureBox6.Name = "PictureBox6"
+        Me.PictureBox6.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox6.TabIndex = 5
+        Me.PictureBox6.TabStop = False
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.Location = New System.Drawing.Point(177, 81)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 4
+        Me.PictureBox5.TabStop = False
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.Location = New System.Drawing.Point(57, 81)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 3
+        Me.PictureBox4.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Location = New System.Drawing.Point(117, 25)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 2
+        Me.PictureBox3.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(177, 25)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(57, 25)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(54, 50)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'ImageList
+        '
+        Me.ImageList.ImageStream = CType(resources.GetObject("ImageList.ImageStream"), System.Windows.Forms.ImageListStreamer)
+        Me.ImageList.TransparentColor = System.Drawing.Color.Transparent
+        Me.ImageList.Images.SetKeyName(0, "cardback.png")
+        Me.ImageList.Images.SetKeyName(1, "circle.png")
+        Me.ImageList.Images.SetKeyName(2, "cross.png")
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(118, 239)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 10
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(318, 284)
+        Me.Controls.Add(Me.btnStart)
+        Me.Controls.Add(Me.grpbJuego)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.grpbJuego.ResumeLayout(False)
+        CType(Me.PictureBox9, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox8, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox7, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox6, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents grpbJuego As System.Windows.Forms.GroupBox
+    Friend WithEvents PictureBox9 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox8 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox7 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox6 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents ImageList As System.Windows.Forms.ImageList
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+
+End Class
---
+++ Eval1/coding/tresEnRaya/tresEnRaya/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+    Dim turn As Integer
+    Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
+        initialization()
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        initialization()
+
+    End Sub
+
+    Private Sub initialization()
+        turn = 1
+        For Each ctrlActual As PictureBox In grpbJuego.Controls
+            ctrlActual.Tag = 0
+            ctrlActual.Image = ImageList.Images(0)
+        Next
+    End Sub
+
+
+
+    Private Sub pic_click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles PictureBox1.Click, PictureBox2.Click, PictureBox3.Click, PictureBox4.Click, PictureBox5.Click, PictureBox6.Click, PictureBox7.Click, PictureBox8.Click, PictureBox9.Click
+        Dim picBoxActual As PictureBox = sender
+        cardAction(picBoxActual)
+        checkEnd()
+    End Sub
+
+
+    Private Sub cardAction(ByVal picBoxActual As PictureBox)
+
+        If picBoxActual.Tag = 0 Then
+            picBoxActual.Image = ImageList.Images(turn)
+            picBoxActual.Tag = turn
+            cambioTurno()
+        End If
+    End Sub
+
+    Private Sub cambioTurno()
+        If turn = 1 Then
+            turn = 2
+        Else
+            turn = 1
+        End If
+    End Sub
+
+
+    Private Sub checkEnd()
+
+
+        If PictureBox1.Tag = 1 And PictureBox2.Tag = 1 And PictureBox3.Tag = 1 Or _
+            PictureBox1.Tag = 1 And PictureBox4.Tag = 1 And PictureBox7.Tag = 1 Or _
+            PictureBox1.Tag = 1 And PictureBox5.Tag = 1 And PictureBox9.Tag = 1 Or _
+            PictureBox2.Tag = 1 And PictureBox5.Tag = 1 And PictureBox8.Tag = 1 Or _
+            PictureBox7.Tag = 1 And PictureBox8.Tag = 1 And PictureBox9.Tag = 1 Or _
+            PictureBox4.Tag = 1 And PictureBox5.Tag = 1 And PictureBox6.Tag = 1 Or _
+            PictureBox7.Tag = 1 And PictureBox5.Tag = 1 And PictureBox3.Tag = 1 Or _
+            PictureBox3.Tag = 1 And PictureBox6.Tag = 1 And PictureBox9.Tag = 1 Then
+
+            MsgBox("Ha ganado el jugador 1")
+            initialization()
+
+        End If
+
+        If PictureBox1.Tag = 2 And PictureBox2.Tag = 2 And PictureBox3.Tag = 2 Or _
+            PictureBox1.Tag = 2 And PictureBox4.Tag = 2 And PictureBox7.Tag = 2 Or _
+            PictureBox1.Tag = 2 And PictureBox5.Tag = 2 And PictureBox9.Tag = 2 Or _
+            PictureBox2.Tag = 2 And PictureBox5.Tag = 2 And PictureBox8.Tag = 2 Or _
+            PictureBox7.Tag = 2 And PictureBox8.Tag = 2 And PictureBox9.Tag = 2 Or _
+            PictureBox4.Tag = 2 And PictureBox5.Tag = 2 And PictureBox6.Tag = 2 Or _
+            PictureBox7.Tag = 2 And PictureBox5.Tag = 2 And PictureBox3.Tag = 2 Or _
+            PictureBox3.Tag = 2 And PictureBox6.Tag = 2 And PictureBox9.Tag = 2 Then
+
+            MsgBox("Ha ganado el jugador 2")
+            initialization()
+
+        End If
+
+    End Sub
+End Class
