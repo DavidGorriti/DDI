99e759a4bc93f3d765bd0fb22e3fdb60b97f29ed
uesteibar <uesteibar@gmail.com>
2014-10-08 11:16:18 +0200

    EjercicioBasico3.3 empezado

---
+++ Eval1/coding/EjercicioBasico3.3/EjercicioBasico3.3/Form1.Designer.vb
@@ -0,0 +1,193 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnVerde = New System.Windows.Forms.Button()
+        Me.btnCourier = New System.Windows.Forms.Button()
+        Me.btnMenosTam = New System.Windows.Forms.Button()
+        Me.btnAzul = New System.Windows.Forms.Button()
+        Me.btnMasTam = New System.Windows.Forms.Button()
+        Me.btnNegro = New System.Windows.Forms.Button()
+        Me.btnRojo = New System.Windows.Forms.Button()
+        Me.btnNegrita = New System.Windows.Forms.Button()
+        Me.btnArial = New System.Windows.Forms.Button()
+        Me.Cursiva = New System.Windows.Forms.Button()
+        Me.btnTachada = New System.Windows.Forms.Button()
+        Me.btnSubrayada = New System.Windows.Forms.Button()
+        Me.txtText = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnVerde
+        '
+        Me.btnVerde.Location = New System.Drawing.Point(12, 82)
+        Me.btnVerde.Name = "btnVerde"
+        Me.btnVerde.Size = New System.Drawing.Size(66, 23)
+        Me.btnVerde.TabIndex = 0
+        Me.btnVerde.Text = "Verde"
+        Me.btnVerde.UseVisualStyleBackColor = True
+        '
+        'btnCourier
+        '
+        Me.btnCourier.Location = New System.Drawing.Point(96, 115)
+        Me.btnCourier.Name = "btnCourier"
+        Me.btnCourier.Size = New System.Drawing.Size(66, 23)
+        Me.btnCourier.TabIndex = 1
+        Me.btnCourier.Text = "Courier"
+        Me.btnCourier.UseVisualStyleBackColor = True
+        '
+        'btnMenosTam
+        '
+        Me.btnMenosTam.Location = New System.Drawing.Point(96, 179)
+        Me.btnMenosTam.Name = "btnMenosTam"
+        Me.btnMenosTam.Size = New System.Drawing.Size(66, 23)
+        Me.btnMenosTam.TabIndex = 2
+        Me.btnMenosTam.Text = "-Tamaño"
+        Me.btnMenosTam.UseVisualStyleBackColor = True
+        '
+        'btnAzul
+        '
+        Me.btnAzul.Location = New System.Drawing.Point(12, 147)
+        Me.btnAzul.Name = "btnAzul"
+        Me.btnAzul.Size = New System.Drawing.Size(66, 23)
+        Me.btnAzul.TabIndex = 3
+        Me.btnAzul.Text = "Azul"
+        Me.btnAzul.UseVisualStyleBackColor = True
+        '
+        'btnMasTam
+        '
+        Me.btnMasTam.Location = New System.Drawing.Point(96, 147)
+        Me.btnMasTam.Name = "btnMasTam"
+        Me.btnMasTam.Size = New System.Drawing.Size(66, 23)
+        Me.btnMasTam.TabIndex = 4
+        Me.btnMasTam.Text = "+Tamaño"
+        Me.btnMasTam.UseVisualStyleBackColor = True
+        '
+        'btnNegro
+        '
+        Me.btnNegro.Location = New System.Drawing.Point(12, 179)
+        Me.btnNegro.Name = "btnNegro"
+        Me.btnNegro.Size = New System.Drawing.Size(66, 23)
+        Me.btnNegro.TabIndex = 5
+        Me.btnNegro.Text = "Negro"
+        Me.btnNegro.UseVisualStyleBackColor = True
+        '
+        'btnRojo
+        '
+        Me.btnRojo.Location = New System.Drawing.Point(12, 115)
+        Me.btnRojo.Name = "btnRojo"
+        Me.btnRojo.Size = New System.Drawing.Size(66, 23)
+        Me.btnRojo.TabIndex = 6
+        Me.btnRojo.Text = "Rojo"
+        Me.btnRojo.UseVisualStyleBackColor = True
+        '
+        'btnNegrita
+        '
+        Me.btnNegrita.Location = New System.Drawing.Point(180, 82)
+        Me.btnNegrita.Name = "btnNegrita"
+        Me.btnNegrita.Size = New System.Drawing.Size(66, 23)
+        Me.btnNegrita.TabIndex = 7
+        Me.btnNegrita.Text = "Negrita"
+        Me.btnNegrita.UseVisualStyleBackColor = True
+        '
+        'btnArial
+        '
+        Me.btnArial.Location = New System.Drawing.Point(96, 82)
+        Me.btnArial.Name = "btnArial"
+        Me.btnArial.Size = New System.Drawing.Size(66, 23)
+        Me.btnArial.TabIndex = 8
+        Me.btnArial.Text = "Arial"
+        Me.btnArial.UseVisualStyleBackColor = True
+        '
+        'Cursiva
+        '
+        Me.Cursiva.Location = New System.Drawing.Point(180, 115)
+        Me.Cursiva.Name = "Cursiva"
+        Me.Cursiva.Size = New System.Drawing.Size(66, 23)
+        Me.Cursiva.TabIndex = 9
+        Me.Cursiva.Text = "Cursiva"
+        Me.Cursiva.UseVisualStyleBackColor = True
+        '
+        'btnTachada
+        '
+        Me.btnTachada.Location = New System.Drawing.Point(180, 147)
+        Me.btnTachada.Name = "btnTachada"
+        Me.btnTachada.Size = New System.Drawing.Size(66, 23)
+        Me.btnTachada.TabIndex = 10
+        Me.btnTachada.Text = "Tachada"
+        Me.btnTachada.UseVisualStyleBackColor = True
+        '
+        'btnSubrayada
+        '
+        Me.btnSubrayada.Location = New System.Drawing.Point(180, 179)
+        Me.btnSubrayada.Name = "btnSubrayada"
+        Me.btnSubrayada.Size = New System.Drawing.Size(66, 23)
+        Me.btnSubrayada.TabIndex = 11
+        Me.btnSubrayada.Text = "Subrayada"
+        Me.btnSubrayada.UseVisualStyleBackColor = True
+        '
+        'txtText
+        '
+        Me.txtText.Location = New System.Drawing.Point(12, 29)
+        Me.txtText.Name = "txtText"
+        Me.txtText.Size = New System.Drawing.Size(235, 20)
+        Me.txtText.TabIndex = 12
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(259, 218)
+        Me.Controls.Add(Me.txtText)
+        Me.Controls.Add(Me.btnSubrayada)
+        Me.Controls.Add(Me.btnTachada)
+        Me.Controls.Add(Me.Cursiva)
+        Me.Controls.Add(Me.btnArial)
+        Me.Controls.Add(Me.btnNegrita)
+        Me.Controls.Add(Me.btnRojo)
+        Me.Controls.Add(Me.btnNegro)
+        Me.Controls.Add(Me.btnMasTam)
+        Me.Controls.Add(Me.btnAzul)
+        Me.Controls.Add(Me.btnMenosTam)
+        Me.Controls.Add(Me.btnCourier)
+        Me.Controls.Add(Me.btnVerde)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnVerde As System.Windows.Forms.Button
+    Friend WithEvents btnCourier As System.Windows.Forms.Button
+    Friend WithEvents btnMenosTam As System.Windows.Forms.Button
+    Friend WithEvents btnAzul As System.Windows.Forms.Button
+    Friend WithEvents btnMasTam As System.Windows.Forms.Button
+    Friend WithEvents btnNegro As System.Windows.Forms.Button
+    Friend WithEvents btnRojo As System.Windows.Forms.Button
+    Friend WithEvents btnNegrita As System.Windows.Forms.Button
+    Friend WithEvents btnArial As System.Windows.Forms.Button
+    Friend WithEvents Cursiva As System.Windows.Forms.Button
+    Friend WithEvents btnTachada As System.Windows.Forms.Button
+    Friend WithEvents btnSubrayada As System.Windows.Forms.Button
+    Friend WithEvents txtText As System.Windows.Forms.TextBox
+
+End Class
---
+++ Eval1/coding/EjercicioBasico3.3/EjercicioBasico3.3/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+
+    Private Sub btnVerde_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnVerde.Click
+        txtText.ForeColor = Color.Green
+    End Sub
+
+    Private Sub btnRojo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRojo.Click
+        txtText.ForeColor = Color.Red
+    End Sub
+
+    Private Sub btnAzul_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnAzul.Click
+        txtText.ForeColor = Color.Blue
+    End Sub
+
+    Private Sub btnNegro_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNegro.Click
+        txtText.ForeColor = Color.Black
+    End Sub
+
+    Private Sub btnArial_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnArial.Click
+        Dim f As New System.Drawing.Font("Arial", txtText.Font.Size)
+        txtText.Font = f
+    End Sub
+
+    Private Sub btnCourier_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCourier.Click
+        Dim f As New System.Drawing.Font("Courier", txtText.Font.Size)
+        txtText.Font = f
+    End Sub
+
+
+    Private Sub btnMasTam_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMasTam.Click
+        Dim f As New System.Drawing.Font(txtText.Font.Name, txtText.Font.Size + 1)
+        txtText.Font = f
+    End Sub
+
+    Private Sub btnMenosTam_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnMenosTam.Click
+        Dim f As New System.Drawing.Font(txtText.Font.Name, txtText.Font.Size - 1)
+        txtText.Font = f
+    End Sub
+
+    Private Sub btnNegrita_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnNegrita.Click
+
+    End Sub
+End Class
