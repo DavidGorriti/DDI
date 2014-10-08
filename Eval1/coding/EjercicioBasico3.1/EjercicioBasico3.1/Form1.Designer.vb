c5c7591dad96a249b907f615b15e328c81637b24
uesteibar <uesteibar@gmail.com>
2014-10-08 10:48:18 +0200

    EjercicioBasico3.1 terminado

---
+++ Eval1/coding/EjercicioBasico3.1/EjercicioBasico3.1/Form1.Designer.vb
@@ -0,0 +1,107 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.txtOutput = New System.Windows.Forms.TextBox()
+        Me.btnPesetas = New System.Windows.Forms.Button()
+        Me.btnEuros = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Cantidad"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 183)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(55, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Resultado"
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(101, 34)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(100, 20)
+        Me.txtInput.TabIndex = 2
+        '
+        'txtOutput
+        '
+        Me.txtOutput.Location = New System.Drawing.Point(101, 180)
+        Me.txtOutput.Name = "txtOutput"
+        Me.txtOutput.Size = New System.Drawing.Size(100, 20)
+        Me.txtOutput.TabIndex = 3
+        '
+        'btnPesetas
+        '
+        Me.btnPesetas.Location = New System.Drawing.Point(39, 105)
+        Me.btnPesetas.Name = "btnPesetas"
+        Me.btnPesetas.Size = New System.Drawing.Size(75, 23)
+        Me.btnPesetas.TabIndex = 4
+        Me.btnPesetas.Text = "A Pesetas"
+        Me.btnPesetas.UseVisualStyleBackColor = True
+        '
+        'btnEuros
+        '
+        Me.btnEuros.Location = New System.Drawing.Point(151, 105)
+        Me.btnEuros.Name = "btnEuros"
+        Me.btnEuros.Size = New System.Drawing.Size(75, 23)
+        Me.btnEuros.TabIndex = 5
+        Me.btnEuros.Text = "A Euros"
+        Me.btnEuros.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.btnEuros)
+        Me.Controls.Add(Me.btnPesetas)
+        Me.Controls.Add(Me.txtOutput)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtInput As System.Windows.Forms.TextBox
+    Friend WithEvents txtOutput As System.Windows.Forms.TextBox
+    Friend WithEvents btnPesetas As System.Windows.Forms.Button
+    Friend WithEvents btnEuros As System.Windows.Forms.Button
+
+End Class
---
+++ Eval1/coding/EjercicioBasico3.1/EjercicioBasico3.1/Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+
+    Private Sub btnPesetas_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnPesetas.Click
+        If IsNumeric(txtInput.Text) Then
+            txtOutput.Text = txtInput.Text * 166
+        End If
+    End Sub
+
+    Private Sub btnEuros_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnEuros.Click
+        If IsNumeric(txtInput.Text) Then
+            txtOutput.Text = txtInput.Text / 166
+        End If
+    End Sub
+End Class
