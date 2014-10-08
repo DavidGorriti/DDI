6fc8a68e5c3ea40e86e07f364de9c217b85c9fee
uesteibar <uesteibar@gmail.com>
2014-10-08 12:35:57 +0200

    PongGame empezado

---
+++ Eval1/coding/PongGame/PongGame/Form1.Designer.vb
@@ -0,0 +1,139 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnStart = New System.Windows.Forms.Button()
+        Me.btnStop = New System.Windows.Forms.Button()
+        Me.pnlPlayground = New System.Windows.Forms.Panel()
+        Me.pctPaddle = New System.Windows.Forms.PictureBox()
+        Me.pctBall = New System.Windows.Forms.PictureBox()
+        Me.lblScore = New System.Windows.Forms.Label()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.pnlPlayground.SuspendLayout()
+        CType(Me.pctPaddle, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pctBall, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnStart
+        '
+        Me.btnStart.Location = New System.Drawing.Point(78, 9)
+        Me.btnStart.Name = "btnStart"
+        Me.btnStart.Size = New System.Drawing.Size(75, 23)
+        Me.btnStart.TabIndex = 0
+        Me.btnStart.Text = "Start"
+        Me.btnStart.UseVisualStyleBackColor = True
+        '
+        'btnStop
+        '
+        Me.btnStop.Enabled = False
+        Me.btnStop.Location = New System.Drawing.Point(179, 9)
+        Me.btnStop.Name = "btnStop"
+        Me.btnStop.Size = New System.Drawing.Size(75, 23)
+        Me.btnStop.TabIndex = 1
+        Me.btnStop.Text = "Stop"
+        Me.btnStop.UseVisualStyleBackColor = True
+        '
+        'pnlPlayground
+        '
+        Me.pnlPlayground.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.pnlPlayground.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnlPlayground.Controls.Add(Me.pctPaddle)
+        Me.pnlPlayground.Controls.Add(Me.pctBall)
+        Me.pnlPlayground.Cursor = System.Windows.Forms.Cursors.Default
+        Me.pnlPlayground.Location = New System.Drawing.Point(12, 88)
+        Me.pnlPlayground.Name = "pnlPlayground"
+        Me.pnlPlayground.Size = New System.Drawing.Size(372, 211)
+        Me.pnlPlayground.TabIndex = 3
+        '
+        'pctPaddle
+        '
+        Me.pctPaddle.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.pctPaddle.Location = New System.Drawing.Point(343, 81)
+        Me.pctPaddle.Name = "pctPaddle"
+        Me.pctPaddle.Size = New System.Drawing.Size(14, 50)
+        Me.pctPaddle.TabIndex = 1
+        Me.pctPaddle.TabStop = False
+        '
+        'pctBall
+        '
+        Me.pctBall.Image = CType(resources.GetObject("pctBall.Image"), System.Drawing.Image)
+        Me.pctBall.Location = New System.Drawing.Point(3, 97)
+        Me.pctBall.Name = "pctBall"
+        Me.pctBall.Size = New System.Drawing.Size(21, 21)
+        Me.pctBall.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.pctBall.TabIndex = 0
+        Me.pctBall.TabStop = False
+        '
+        'lblScore
+        '
+        Me.lblScore.AutoSize = True
+        Me.lblScore.Font = New System.Drawing.Font("Microsoft Sans Serif", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblScore.Location = New System.Drawing.Point(104, 26)
+        Me.lblScore.Name = "lblScore"
+        Me.lblScore.Size = New System.Drawing.Size(0, 46)
+        Me.lblScore.TabIndex = 4
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(309, 9)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 5
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(396, 311)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.lblScore)
+        Me.Controls.Add(Me.pnlPlayground)
+        Me.Controls.Add(Me.btnStop)
+        Me.Controls.Add(Me.btnStart)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.pnlPlayground.ResumeLayout(False)
+        CType(Me.pctPaddle, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pctBall, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnStart As System.Windows.Forms.Button
+    Friend WithEvents btnStop As System.Windows.Forms.Button
+    Friend WithEvents pnlPlayground As System.Windows.Forms.Panel
+    Friend WithEvents lblScore As System.Windows.Forms.Label
+    Friend WithEvents pctPaddle As System.Windows.Forms.PictureBox
+    Friend WithEvents pctBall As System.Windows.Forms.PictureBox
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Timer1 As System.Windows.Forms.Timer
+
+End Class
---
+++ Eval1/coding/PongGame/PongGame/Form1.vb
@@ -0,0 +1,79 @@
+﻿Public Class Form1
+
+    Dim Xi As Integer
+    Dim Yi As Integer
+
+    Private Sub btnExit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub pnlPlayground_MouseEnter(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pnlPlayground.MouseEnter
+        Cursor.Hide()
+    End Sub
+
+    Private Sub pnlPlayground_MouseLeave(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pnlPlayground.MouseLeave
+        Cursor.Show()
+    End Sub
+
+
+    Private Sub pnlPlayground_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles pnlPlayground.MouseMove
+        If e.Y > pnlPlayground.Height - pctPaddle.Height Then
+
+            pctPaddle.Location = New Point(pctPaddle.Location.X, pnlPlayground.Height - pctPaddle.Height)
+
+        Else
+
+            pctPaddle.Location = New Point(pctPaddle.Location.X, e.Y)
+
+        End If
+
+
+    End Sub
+
+    Private Sub btnStart_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStart.Click
+        pctPaddle.Location = New Point(pctPaddle.Location.X, (pnlPlayground.Height / 2) - (pctPaddle.Height / 2))
+
+        btnStart.Enabled = False
+        btnStop.Enabled = True
+
+        Dim rnd As Random = New Random
+        Dim direction As Integer
+
+        pctBall.Location = New Point(10, rnd.Next(pnlPlayground.Height - pctBall.Height))
+
+        Xi = 10
+        direction = rnd.Next(3)
+
+        Select Case direction
+            Case 0
+                Yi = -10
+            Case 1
+                Yi = 0
+            Case 2
+                Yi = 10
+        End Select
+
+
+        Timer1.Start()
+
+
+
+
+
+    End Sub
+
+    Private Sub btnStop_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnStop.Click
+        btnStart.Enabled = True
+        btnStop.Enabled = False
+
+        Timer1.Stop()
+
+    End Sub
+
+
+
+
+    Private Sub Timer1_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Timer1.Tick
+        pctBall.Location = New Point(pctBall.Location.X + Xi, pctBall.Location.Y + Yi)
+    End Sub
+End Class
