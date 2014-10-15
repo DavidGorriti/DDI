d0f287651f2ac76a91a29fa7798c745612eb481a
uesteibar <uesteibar@gmail.com>
2014-10-15 12:04:43 +0200

    Ejercicio3.5 hecho

--- Eval1/coding/EjercicioBasico3.5/EjercicioBasico3.5/Form1.Designer.vb
+++ Eval1/coding/EjercicioBasico3.5/EjercicioBasico3.5/Form1.Designer.vb
@@ -22,9 +22,73 @@ Partial Class Form1
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.hscBarra = New System.Windows.Forms.HScrollBar()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.txtF = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'hscBarra
+        '
+        Me.hscBarra.Location = New System.Drawing.Point(9, 55)
+        Me.hscBarra.Minimum = -100
+        Me.hscBarra.Name = "hscBarra"
+        Me.hscBarra.Size = New System.Drawing.Size(266, 44)
+        Me.hscBarra.TabIndex = 0
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(41, 149)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(25, 20)
+        Me.txtC.TabIndex = 1
+        '
+        'txtF
+        '
+        Me.txtF.Location = New System.Drawing.Point(227, 149)
+        Me.txtF.Name = "txtF"
+        Me.txtF.Size = New System.Drawing.Size(25, 20)
+        Me.txtF.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 152)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(18, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cº"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(203, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(17, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Fº"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtF)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.hscBarra)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents hscBarra As System.Windows.Forms.HScrollBar
+    Friend WithEvents txtC As System.Windows.Forms.TextBox
+    Friend WithEvents txtF As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
 
 End Class
--- Eval1/coding/EjercicioBasico3.5/EjercicioBasico3.5/Form1.vb
+++ Eval1/coding/EjercicioBasico3.5/EjercicioBasico3.5/Form1.vb
@@ -1,3 +1,17 @@
 ﻿Public Class Form1
 
+    Private Sub hscBarra_Scroll(ByVal sender As System.Object, ByVal e As System.Windows.Forms.ScrollEventArgs) Handles hscBarra.Scroll
+        Dim bar As HScrollBar = sender
+
+        txtC.Text = bar.Value
+        txtF.Text = bar.Value * 1.8 + 32
+
+        If bar.Value < -10 Then
+            Me.BackColor = Color.Blue
+        ElseIf bar.Value > 10 Then
+            Me.BackColor = Color.Red
+        Else
+            Me.BackColor = Color.Orange
+        End If
+    End Sub
 End Class
