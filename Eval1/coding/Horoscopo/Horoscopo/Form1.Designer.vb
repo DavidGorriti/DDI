77ed526c2a7317a64d0af1a01061962ab4df2990
uesteibar <uesteibar@gmail.com>
2014-10-30 10:17:05 +0100

    Ejercicio Horoscopo empezado

---
+++ Eval1/coding/Horoscopo/Horoscopo/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtDia = New System.Windows.Forms.TextBox()
+        Me.txtMes = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(149, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(23, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Dia"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(149, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(27, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Mes"
+        '
+        'txtDia
+        '
+        Me.txtDia.Location = New System.Drawing.Point(192, 16)
+        Me.txtDia.Name = "txtDia"
+        Me.txtDia.Size = New System.Drawing.Size(35, 20)
+        Me.txtDia.TabIndex = 2
+        '
+        'txtMes
+        '
+        Me.txtMes.Location = New System.Drawing.Point(192, 42)
+        Me.txtMes.Name = "txtMes"
+        Me.txtMes.Size = New System.Drawing.Size(35, 20)
+        Me.txtMes.TabIndex = 3
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 100)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(392, 226)
+        Me.TextBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(416, 339)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtMes)
+        Me.Controls.Add(Me.txtDia)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents txtDia As System.Windows.Forms.TextBox
+    Friend WithEvents txtMes As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+
+End Class
---
+++ Eval1/coding/Horoscopo/Horoscopo/ModHoroscopo.vb
@@ -0,0 +1,89 @@
+﻿Module ModHoroscopo
+
+    Public Function fechaValida(ByVal mes As Integer, ByVal dia As Integer) As Boolean
+        Select Case mes
+            Case 1, 3, 5, 7, 8, 10, 12
+                If dia > 0 And dia <= 31 Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Case 4, 6, 9, 11
+                If dia > 0 And dia <= 30 Then
+                    Return True
+                Else
+                    Return False
+                End If
+            Case 2
+                If dia > 0 And dia <= 28 Then
+                    Return True
+                Else
+                    Return False
+                End If
+        End Select
+    End Function
+
+
+    Public Sub muestraHoroscopo(ByVal mes As Integer, ByVal dia As Integer, ByVal txtBox As TextBox)
+        If fechaValida(mes, dia) Then
+            txtBox.Text = getHoroscopoText(mes, dia)
+        End If
+    End Sub
+
+    Private Function getHoroscopoText(ByVal mes As Integer, ByVal dia As Integer) As String
+        Dim aries As String = "Personas fuertes, con energía, institivos y dinámicos... con coraje. a veces egocentristas, tienden a acaparar el liderazgo, signo vital asociado a grandes deportistas de competición."
+        Dim tauro As String = "Personas resistentes, pacientes, que saben guardar la calma. Tienen como activo la inteligencia. Son personas muy realistas."
+        Dim geminis As String = "Contradictorios y versátiles, cambian con facilidad. Pasan de ser serenos, y fascinantes, a pesimistas y amorfos. Fantasiosos, aman la libertad e independencia."
+        Dim cancer As String = "Familares, tímidos, posesivos, imaginativos y románticos. Dulces que viajan entre la melancolía y la alegría."
+        Dim leo As String = "Extrovertidos, autoritarios,libres, fuertes, pasionales y generosos. Optimistas y a veces un poco imprudentes."
+        Dim virgo As String = "Meticulosos en los detalles, eficientes, severos y racionales en su vida y trabajo. Buenos ahorradores."
+        Dim libra As String = "Diplomatico, encantador y sociable. Los libra son idealistas, pacíficos, optimistas y románticos."
+        Dim escorpio As String = "Los más intuitivos, tienden a ser profundos y serios, autoritario, celoso, posesivo pero mutables y pasionales."
+        Dim sagitario As String = "Confiados, alegres, sinceros, fieles amigos, amantes de la naturaleza y el aire libre."
+        Dim capricornio As String = "Son introvertidos impulsivos... Rasgos en ellos son la timidez y la inseguridad; perfil ambicioso, frío, melancólico, pero son afectuosos."
+        Dim acuario As String = "Sinceros, altruistas, simpáticos y activos, pero pasan por momentos de gran nerviosismo y tensión, vanidosos, muy creativos, agradables."
+        Dim piscis As String = "Sutiles, dulces, tolerantes, imprevisibles, románticos pero quisquillosos e incluso infieles. Creen en la amistad y el amor."
+
+        Select Case mes
+            Case 1
+                If mes <= 19 Then
+                    Return capricornio
+                Else
+                    Return acuario
+                End If
+            Case 2
+                If mes <= 18 Then
+                    Return acuario
+                Else
+                    Return piscis
+                End If
+            Case 3
+                If mes <= 19 Then
+                    Return piscis
+                Else
+                    Return aries
+                End If
+            Case 4
+                If mes <= 18 Then
+                    Return aries
+                Else
+                    Return tauro
+                End If
+            Case 5
+                If mes <= 19 Then
+                    Return tauro
+                Else
+                    Return geminis
+                End If
+            Case 6
+                If mes <= 20 Then
+                    Return geminis
+                Else
+                    Return cancer
+                End If
+
+        End Select
+
+    End Function
+
+End Module
