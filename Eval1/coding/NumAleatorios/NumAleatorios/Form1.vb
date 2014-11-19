4da131b7e5fc9b70f6cb457cb173ca0b0a99261a
uesteibar <uesteibar@gmail.com>
2014-11-19 12:26:54 +0100

    Ejercicio NumAleatorio empezado

---
+++ Eval1/coding/NumAleatorios/NumAleatorios/Form1.vb
@@ -0,0 +1,38 @@
+﻿Public Class Form1
+    Dim arr(1, 1) As Integer
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        ReDim arr(PedirValores("X"), PedirValores("Y"))
+
+        'rellenar array
+
+
+    End Sub
+
+
+
+    Function PedirValores(ByVal mode) As Integer
+        Dim X As Integer = -1
+        Dim Y As Integer = -1
+
+        If mode = "X" Then
+            While X = -1
+                Dim xT As String = InputBox("Introduzca valor horizontal de la matriz", "Valor Horizontal", -1)
+                If IsNumeric(xT) And xT <> -1 Then
+                    X = Val(xT)
+                    Return X
+                End If
+            End While
+        End If
+
+        If mode = "Y" Then
+            While Y = -1
+                Dim yT As String = InputBox("Introduzca valor vertical de la matriz", "Valor Vertical", -1)
+                If IsNumeric(yT) And yT <> -1 Then
+                    Y = Val(yT)
+                    Return Y
+                End If
+            End While
+        End If
+
+    End Function
+End Class
