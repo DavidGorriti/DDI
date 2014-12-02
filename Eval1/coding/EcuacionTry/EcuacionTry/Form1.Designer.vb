16a004510198171287b009127c025995ad564cc0
uesteibar <uesteibar@gmail.com>
2014-12-02 14:20:42 +0100

    Ejercicio TryCatch empezado

---
+++ Eval1/coding/EcuacionTry/EcuacionTry/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtB = New System.Windows.Forms.TextBox()
+        Me.txtA = New System.Windows.Forms.TextBox()
+        Me.txtC = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnCalcular = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.lblSolucion1 = New System.Windows.Forms.Label()
+        Me.lblSolucion2 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(58, 22)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(35, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "X^2 +"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(136, 22)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(23, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "X +"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(204, 22)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "="
+        '
+        'txtB
+        '
+        Me.txtB.Location = New System.Drawing.Point(99, 19)
+        Me.txtB.Name = "txtB"
+        Me.txtB.Size = New System.Drawing.Size(35, 20)
+        Me.txtB.TabIndex = 4
+        '
+        'txtA
+        '
+        Me.txtA.Location = New System.Drawing.Point(20, 19)
+        Me.txtA.Name = "txtA"
+        Me.txtA.Size = New System.Drawing.Size(35, 20)
+        Me.txtA.TabIndex = 5
+        '
+        'txtC
+        '
+        Me.txtC.Location = New System.Drawing.Point(163, 19)
+        Me.txtC.Name = "txtC"
+        Me.txtC.Size = New System.Drawing.Size(35, 20)
+        Me.txtC.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(223, 22)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "0"
+        '
+        'btnCalcular
+        '
+        Me.btnCalcular.Location = New System.Drawing.Point(84, 61)
+        Me.btnCalcular.Name = "btnCalcular"
+        Me.btnCalcular.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalcular.TabIndex = 8
+        Me.btnCalcular.Text = "Calcular"
+        Me.btnCalcular.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(58, 99)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(82, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Solucion 1: X = "
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(58, 125)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(82, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Solucion 2: X = "
+        '
+        'lblSolucion1
+        '
+        Me.lblSolucion1.AutoSize = True
+        Me.lblSolucion1.Location = New System.Drawing.Point(146, 99)
+        Me.lblSolucion1.Name = "lblSolucion1"
+        Me.lblSolucion1.Size = New System.Drawing.Size(13, 13)
+        Me.lblSolucion1.TabIndex = 11
+        Me.lblSolucion1.Text = "0"
+        '
+        'lblSolucion2
+        '
+        Me.lblSolucion2.AutoSize = True
+        Me.lblSolucion2.Location = New System.Drawing.Point(146, 125)
+        Me.lblSolucion2.Name = "lblSolucion2"
+        Me.lblSolucion2.Size = New System.Drawing.Size(13, 13)
+        Me.lblSolucion2.TabIndex = 12
+        Me.lblSolucion2.Text = "0"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(95, 112)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(66, 13)
+        Me.Label7.TabIndex = 13
+        Me.Label7.Text = "Sin Solución"
+        Me.Label7.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(251, 147)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lblSolucion2)
+        Me.Controls.Add(Me.lblSolucion1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.btnCalcular)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtC)
+        Me.Controls.Add(Me.txtA)
+        Me.Controls.Add(Me.txtB)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents txtB As System.Windows.Forms.TextBox
+    Friend WithEvents txtA As System.Windows.Forms.TextBox
+    Friend WithEvents txtC As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents btnCalcular As System.Windows.Forms.Button
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents lblSolucion1 As System.Windows.Forms.Label
+    Friend WithEvents lblSolucion2 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ Eval1/coding/EcuacionTry/EcuacionTry/Form1.vb
@@ -0,0 +1,32 @@
+﻿Public Class Form1
+
+
+    Private Sub btnCalcular_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnCalcular.Click
+        Dim a, b, c As Integer
+
+        a = Int(txtA.Text)
+        b = Int(txtB.Text)
+        c = Int(txtC.Text)
+
+        Dim x1, x2 As Double
+
+        Try
+            x1 = (-b + Math.Sqrt((b ^ 2) - 4 * a * c)) / 2 * a
+            x2 = (-b + Math.Sqrt((b ^ 2) - 4 * a * c)) / 2 * a
+
+
+        Catch ex As Exception
+
+
+        End Try
+        
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        txtA.Text = 0
+        txtB.Text = 0
+        txtC.Text = 0
+    End Sub
+End Class
