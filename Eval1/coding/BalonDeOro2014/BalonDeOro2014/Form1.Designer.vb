a1bf26b270a2ef5577b2a16c590570610ec71c77
uesteibar <uesteibar@gmail.com>
2015-01-21 10:49:36 +0100

    Ejercicio Balon de oro terminado

---
+++ Eval1/coding/BalonDeOro2014/BalonDeOro2014/Form1.Designer.vb
@@ -0,0 +1,78 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblOro = New System.Windows.Forms.Label()
+        Me.lblPlata = New System.Windows.Forms.Label()
+        Me.lblBronce = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblOro
+        '
+        Me.lblOro.AutoSize = True
+        Me.lblOro.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!)
+        Me.lblOro.Location = New System.Drawing.Point(144, 62)
+        Me.lblOro.Name = "lblOro"
+        Me.lblOro.Size = New System.Drawing.Size(0, 36)
+        Me.lblOro.TabIndex = 0
+        Me.lblOro.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblPlata
+        '
+        Me.lblPlata.AutoSize = True
+        Me.lblPlata.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!)
+        Me.lblPlata.Location = New System.Drawing.Point(144, 113)
+        Me.lblPlata.Name = "lblPlata"
+        Me.lblPlata.Size = New System.Drawing.Size(0, 36)
+        Me.lblPlata.TabIndex = 1
+        Me.lblPlata.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'lblBronce
+        '
+        Me.lblBronce.AutoSize = True
+        Me.lblBronce.Font = New System.Drawing.Font("Microsoft Sans Serif", 22.0!)
+        Me.lblBronce.Location = New System.Drawing.Point(144, 164)
+        Me.lblBronce.Name = "lblBronce"
+        Me.lblBronce.Size = New System.Drawing.Size(0, 36)
+        Me.lblBronce.TabIndex = 2
+        Me.lblBronce.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(700, 262)
+        Me.Controls.Add(Me.lblBronce)
+        Me.Controls.Add(Me.lblPlata)
+        Me.Controls.Add(Me.lblOro)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblOro As System.Windows.Forms.Label
+    Friend WithEvents lblPlata As System.Windows.Forms.Label
+    Friend WithEvents lblBronce As System.Windows.Forms.Label
+
+End Class
---
+++ Eval1/coding/BalonDeOro2014/BalonDeOro2014/Form1.vb
@@ -0,0 +1,70 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+
+        Dim jugadores(23) As Integer
+        Dim i As Integer
+
+        For i = 0 To 22
+            jugadores(i) = 0
+        Next
+
+        Try
+            Dim conexion As New OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=|DataDirectory|\BalonDeOro2014.mdb;")
+            conexion.Open()
+
+            Dim cmd As New OleDb.OleDbCommand
+            cmd.Connection = conexion
+            cmd.CommandType = CommandType.Text
+            cmd.CommandText = "SELECT * FROM Votantes"
+
+            Dim dr As OleDb.OleDbDataReader
+
+            dr = cmd.ExecuteReader
+
+            While dr.Read
+                jugadores(dr("Primero") - 1) += 5
+                jugadores(dr("Segundo") - 1) += 3
+                jugadores(dr("Tercero") - 1) += 1
+            End While
+
+            dr.Close()
+
+            Dim ds As New DataSet()
+            Dim da As New OleDb.OleDbDataAdapter
+
+            Dim com As New OleDb.OleDbCommand("SELECT * FROM Jugadores")
+            da.SelectCommand = com
+            da.SelectCommand.Connection = conexion
+
+            'crear commandbuilder
+            Dim cb As OleDb.OleDbCommandBuilder = New OleDb.OleDbCommandBuilder(da)
+
+            da.Fill(ds, "Jugadores")
+
+            For i = 0 To 22
+                ds.Tables("Jugadores")(i)("Votos") = jugadores(i)
+            Next
+
+            Dim dv As New DataView(ds.Tables("Jugadores"))
+            dv.Sort = "Votos DESC"
+
+            lblOro.Text = dv(0)("Apellido") & "-" & dv(0)("Votos")
+            lblPlata.Text = dv(1)("Apellido") & "-" & dv(1)("Votos")
+            lblBronce.Text = dv(2)("Apellido") & "-" & dv(2)("Votos")
+
+            'ds.Tables("Jugadores").PrimaryKey = {ds.Tables("Jugadores").Columns("Id")}
+            'cb.GetUpdateCommand()
+            da.Update(ds, "Jugadores")
+
+            conexion.Close()
+        Catch ex As Exception
+            MsgBox("Error de conexion:" & ex.Message)
+            Me.Close()
+        End Try
+
+    End Sub
+
+
+End Class
+
